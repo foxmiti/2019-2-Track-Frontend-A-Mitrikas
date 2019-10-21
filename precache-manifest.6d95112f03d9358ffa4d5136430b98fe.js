563e781423b2f44fc7430fc337ef5e2896bcd1c8
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-10-21 12:43:34 +0000

    Deploy foxmiti/2019-2-Track-Frontend-A-Mitrikas to github.com/foxmiti/2019-2-Track-Frontend-A-Mitrikas.git:gh-pages

---
+++ precache-manifest.6d95112f03d9358ffa4d5136430b98fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74f7bed8f3d5ea30f2047439836f02b3",
+    "url": "/2019-2-Track-Frontend-A-Mitrikas/index.html"
+  },
+  {
+    "revision": "edb94c3e280aaa98888f",
+    "url": "/2019-2-Track-Frontend-A-Mitrikas/static/css/main.b4eae362.chunk.css"
+  },
+  {
+    "revision": "b5b183f5e517863710d4",
+    "url": "/2019-2-Track-Frontend-A-Mitrikas/static/js/2.f90ea1e5.chunk.js"
+  },
+  {
+    "revision": "edb94c3e280aaa98888f",
+    "url": "/2019-2-Track-Frontend-A-Mitrikas/static/js/main.12b109a5.chunk.js"
+  },
+  {
+    "revision": "e0b9d485aaf05c8287af",
+    "url": "/2019-2-Track-Frontend-A-Mitrikas/static/js/runtime-main.be72c736.js"
+  },
+  {
+    "revision": "795fb555bea801821f51202dc10aaa78",
+    "url": "/2019-2-Track-Frontend-A-Mitrikas/static/media/logo.795fb555.svg"
+  }
+]);
